7b00e1b2ae315a267068253e21dd8d91de724a1b
Joseph <afiliateejoseph@gmail.com>
2024-08-16 15:38:21 +0100

    json server working

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT);
+
+// const requestLogger = (request, response, next) => {
+//   console.log("Method:", request.method);
+//   console.log("Path:  ", request.path);
+//   console.log("Body:  ", request.body);
+//   console.log("---");
+//   next();
+// };
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(requestLogger);
+
+// let users = [];
+
+// app.get("/api/users", (req, res) => {
+//   console.log("now");
+
+//   res.json(users);
+// });
+
+// app.get("/api/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const user = users.find((user) => user.id === id);
+
+//   if (!user) {
+//     res.statusMessage = `Sorry, user of id ${id} does not exist`;
+//     res.status(404).end();
+//   }
+//   res.json(user);
+// });
+
+// const generateId = () => {
+//   const maxId =
+//     users.length > 0 ? Math.max(...users.map((n) => Number(n.id))) : 0;
+
+//   return String(maxId + 1);
+// };
+
+// // Create a new user (NO POST REQUEST IS BEING USED)
+// app.post("/api/users", (req, res) => {
+//   const body = req.body;
+//   if (!body) {
+//     return res.status(400).json({
+//       error: "Content Missing",
+//     });
+//   }
+
+//   const user = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   };
+
+//   users = users.concat(user);
+
+//   res.json(user);
+// });
+
+// // Update User
+// app.put("/api/users/:id", (req, res) => {
+//   // Get the user id
+//   const changedUser = req.body;
+//   const id = req.id;
+//   console.log(changedUser, id);
+// });
+
+// app.delete("/api/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   users = users.filter((user) => user.id !== id);
+
+//   res.status(204).end();
+// });
+
+// const unKnownEndPoint = (req, res) => {
+//   res.status(404).end({ error: "unknown endpoint" });
+// };
+
+// app.use(unKnownEndPoint);
+
+// const PORT = 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
